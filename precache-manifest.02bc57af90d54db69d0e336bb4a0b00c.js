f7215ab07a484497a611c6577895101efa749efd
Hossam <hossam.nasr99@gmail.com>
2018-12-21 19:38:38 -0500

    Updates

---
+++ precache-manifest.02bc57af90d54db69d0e336bb4a0b00c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "959675632bf64cae31ed",
+    "url": "/static/css/main.febf573f.chunk.css"
+  },
+  {
+    "revision": "959675632bf64cae31ed",
+    "url": "/static/js/main.95967563.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "77319f3cb1f0a167e73413984a0e68c4",
+    "url": "/index.html"
+  }
+];
