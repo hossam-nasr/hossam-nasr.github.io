f7b47e9d4809185cd88370f248d8401a94a3bff6
Hossam <hossam.nasr99@gmail.com>
2019-01-05 16:54:04 -0500

    added auto scrolling on path change

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
+import ScrollToTop from "./components/ScrollToTop";
 import HomePage from "./screens/HomePage";
 import ProjectsPage from "./screens/ProjectsPage";
 import ContactPage from "./screens/ContactPage";
@@ -13,14 +14,16 @@ class App extends Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/projects" component={ProjectsPage} />
-            <Route exact path="/contact" component={ContactPage} />
-            <Route exact path="/resume" component={ResumePage} />
-            <Route exact path="/about" component={AboutPage} />
-          </Switch>
+        <Router onUpdate={() => window.scrollTo(0, 0)}>
+          <ScrollToTop>
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route exact path="/projects" component={ProjectsPage} />
+              <Route exact path="/contact" component={ContactPage} />
+              <Route exact path="/resume" component={ResumePage} />
+              <Route exact path="/about" component={AboutPage} />
+            </Switch>
+          </ScrollToTop>
         </Router>
       </ThemeProvider>
     );
