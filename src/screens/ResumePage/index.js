985bc476894a9b4aeb14f67ba89c3b67cb3832b9
Hossam <hossam.nasr99@gmail.com>
2019-01-04 21:54:23 -0500

    added resume page

--- src/screens/ResumePage/index.js
+++ src/screens/ResumePage/index.js
@@ -5,10 +5,103 @@ import Title from "./../../components/Title";
 import MoreButton from "./../../components/MoreButton";
 import Section from "./../../components/Section";
 import { resume } from "./../../constants.js";
-import { SectionContentContainer, ButtonContainer } from "./styles";
+import {
+  SectionContentContainer,
+  ButtonContainer,
+  Subtitle,
+  DetailsContainer,
+  Details,
+  DetailsTitle,
+  InlineDetails,
+  DetailsRight
+} from "./styles";
 
 class ResumePage extends Component {
   render() {
+    const educationDetails = resume.data.education.map(education => (
+      <DetailsContainer key={education.key}>
+        <InlineDetails>
+          <DetailsTitle>{education.title}</DetailsTitle>
+          <DetailsRight>{education.location}</DetailsRight>
+        </InlineDetails>
+        <InlineDetails>
+          <Details>{education.description}</Details>
+          <DetailsRight>{education.end}</DetailsRight>
+        </InlineDetails>
+        {education.gpa && (
+          <Details>
+            <b>GPA</b>: {education.gpa}
+          </Details>
+        )}
+        {education.relevant && (
+          <Details>
+            <b>Relevant Coursework</b>: {education.relevant}
+          </Details>
+        )}
+        {education.abroad && (
+          <InlineDetails>
+            <Details>
+              <b>Study Abroad</b>: {education.abroad.description}
+            </Details>
+            <DetailsRight>
+              {education.abroad.start} - {education.abroad.end}
+            </DetailsRight>
+          </InlineDetails>
+        )}
+      </DetailsContainer>
+    ));
+
+    const techDetails = (
+      <DetailsContainer>
+        <Details>
+          <b>Programming</b>: {resume.data.tech.programming.join(", ")}
+        </Details>
+        <Details>
+          <b>Operating Systems</b>: {resume.data.tech.os.join(", ")}
+        </Details>
+        <Details>
+          <b>Web Development/Design</b>: {resume.data.tech.web.join(", ")}
+        </Details>
+      </DetailsContainer>
+    );
+
+    const leaderDetails = resume.data.leadership.map(leader => (
+      <DetailsContainer key={leader.key}>
+        <InlineDetails>
+          <DetailsTitle>{leader.title}</DetailsTitle>
+          <DetailsRight>{leader.location}</DetailsRight>
+        </InlineDetails>
+        <InlineDetails>
+          <Details>
+            <b>{leader.position}</b>
+          </Details>
+          <DetailsRight>
+            {leader.start} - {leader.current ? "Present" : leader.end}
+          </DetailsRight>
+        </InlineDetails>
+        <Details>
+          <ul>
+            {leader.description.map((content, id) => (
+              <li key={id}>{content}</li>
+            ))}
+          </ul>
+        </Details>
+      </DetailsContainer>
+    ));
+
+    const skillsDetails = (
+      <DetailsContainer>
+        <Details>
+          <b>Languages</b>:{" "}
+          {resume.data.skills.languages
+            .map(l => `${l.name} (${l.level})`)
+            .join(", ")}
+        </Details>
+        <Details>
+          <b>Interests</b>: {resume.data.skills.interests.join(", ")}
+        </Details>
+      </DetailsContainer>
+    );
     return (
       <div>
         <NavigationBar page="Projects" />
@@ -19,22 +112,41 @@ class ResumePage extends Component {
             subtitle={resume.subtitle}
             background={resume.background}
             fontColor={resume.fontColor}
-          />
-          <SectionContentContainer
-            background={resume.background}
-            fontColor={resume.fontColor}
           >
-            <ButtonContainer>
-              <MoreButton
-                primary={resume.fontColor}
-                secondary={resume.background}
-                href="/Resume.pdf"
-                target="_blank"
-              >
-                Download Resume PDF
-              </MoreButton>
-            </ButtonContainer>
-          </SectionContentContainer>
+            <SectionContentContainer
+              background={resume.background}
+              fontcolor={resume.fontColor}
+            >
+              <ButtonContainer>
+                <MoreButton
+                  primary={resume.fontColor}
+                  secondary={resume.background}
+                  href="/Resume.pdf"
+                  target="_blank"
+                >
+                  Download Resume PDF
+                </MoreButton>
+              </ButtonContainer>
+              <Subtitle>Education</Subtitle>
+              {educationDetails}
+              <Subtitle>Technical Skills</Subtitle>
+              {techDetails}
+              <Subtitle>Leadership and Experience</Subtitle>
+              {leaderDetails}
+              <Subtitle>Skills and Interests</Subtitle>
+              {skillsDetails}
+              <ButtonContainer>
+                <MoreButton
+                  primary={resume.fontColor}
+                  secondary={resume.background}
+                  href="/Resume.pdf"
+                  target="_blank"
+                >
+                  Download Resume PDF
+                </MoreButton>
+              </ButtonContainer>
+            </SectionContentContainer>
+          </Section>
         </ContentContainer>
       </div>
     );
--- src/screens/ResumePage/styles.js
+++ src/screens/ResumePage/styles.js
@@ -9,9 +9,62 @@ export const SectionContentContainer = styled(Flex)`
   background-color: ${props => themeGet(`colors.${props.background}`, "black")};
   color: ${props => themeGet(`colors.${props.fontcolor}`, "white")};
   padding-bottom: 35px;
+  margin: 0vh 10vw 0vh 10vw;
 `;
 
 export const ButtonContainer = styled.div`
-  width: 20%;
+  width: 25%;
   text-align: center;
+  margin-bottom: 35px;
+  margin-top: 35px;
+`;
+
+export const Subtitle = styled.div`
+  font-size: 2vmax;
+  font-family: ${themeGet("fonts.primary", "sans-serif")};
+  @media all and (max-width: 500px) {
+    font-size: 4vmax;
+  }
+  margin: 1vh 5vw 2.5vh 5vw;
+  text-align: center;
+`;
+
+export const DetailsContainer = styled(Flex)`
+  flex-flow: column nowrap;
+  font-size: 1.1vmax;
+  align-self: flex-start;
+  font-family: ${themeGet("fonts.primary", "sans-serif")};
+  @media all and (max-width: 500px) {
+    font-size: 2.2vmax;
+  }
+  width: 100%;
+  margin-bottom: 10px;
+`;
+
+export const Details = styled(Flex)`
+  flex-grow: 10;
+  flex-basis: 80%;
+  text-align: left;
+  width: 100%;
+  margin-bottom: 5px;
+`;
+
+export const DetailsRight = styled.div`
+  text-align: right;
+      flex-basis: 15%;
+  flex-shrink: 1;
+`;
+
+export const InlineDetails = styled(Flex)`
+  flex-flow: row nowrap;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+export const DetailsTitle=styled(Details)`
+  font-size: 1.2vmax;
+  font-weight: bold;
+  @media all and (max-width: 500px) {
+      font-size: 1.4vmax;
+  }
 `;
