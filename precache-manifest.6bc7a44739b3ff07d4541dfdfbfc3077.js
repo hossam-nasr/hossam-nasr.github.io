cdbb8d440a1c0e15e2870d5ad37589c51dc3101b
Hossam <hossam.nasr99@gmail.com>
2019-07-09 00:09:25 -0400

    Updates

---
+++ precache-manifest.6bc7a44739b3ff07d4541dfdfbfc3077.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b69c4c847088c7221174705ee14d062",
+    "url": "/index.html"
+  },
+  {
+    "revision": "239556d853b4549b14fd",
+    "url": "/static/css/main.7910c61d.chunk.css"
+  },
+  {
+    "revision": "2781453d5665155b7fe5",
+    "url": "/static/js/2.60d6322d.chunk.js"
+  },
+  {
+    "revision": "239556d853b4549b14fd",
+    "url": "/static/js/main.81bef51b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f4403292eef2cf87cbad34c3f0e729d9",
+    "url": "/static/media/Forms4FriendsLogo.f4403292.png"
+  },
+  {
+    "revision": "057999e2886fe81584c8b99f13200449",
+    "url": "/static/media/TEDxEBIS.057999e2.png"
+  },
+  {
+    "revision": "cb36e9eb992ab4e7deb41ab23504971a",
+    "url": "/static/media/TEDxISEE.cb36e9eb.jpg"
+  },
+  {
+    "revision": "2f7f24e7c488570681a47baa2dc60ae2",
+    "url": "/static/media/aalumlogo.2f7f24e7.png"
+  },
+  {
+    "revision": "cca68f0d0da80d357ae1b44c9b398302",
+    "url": "/static/media/banner.cca68f0d.jpg"
+  },
+  {
+    "revision": "f891d7d4c709a878d537712bc7734f7c",
+    "url": "/static/media/convergelogo.f891d7d4.png"
+  },
+  {
+    "revision": "60b6680c5a89ef244061a0c67727c829",
+    "url": "/static/media/devlogo.60b6680c.png"
+  },
+  {
+    "revision": "bee06e7f4a74275c9011f9e4dd028a2d",
+    "url": "/static/media/lightsourcelogo.bee06e7f.png"
+  },
+  {
+    "revision": "8ea832fb8c31075bcd3b2b017570654a",
+    "url": "/static/media/logo.8ea832fb.png"
+  },
+  {
+    "revision": "bb598af352199e896eced5be19cb315a",
+    "url": "/static/media/thekeylogo.bb598af3.png"
+  },
+  {
+    "revision": "3a053cb118e8984f4b8254a78686a7d0",
+    "url": "/static/media/todolistlogo.3a053cb1.png"
+  }
+]);
