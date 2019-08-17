e3eb113fd73b34edeb412611252d3e1efd637747
Hossam <hossam.nasr99@gmail.com>
2019-08-17 20:42:14 +0200

    added new colors to theme

--- src/theme.js
+++ src/theme.js
@@ -11,7 +11,7 @@ const theme = {
     background: {
       primary: "#e5e5e5",
       secondary: "#2b2b2b",
-      tertiary: '#eaf2ff',
+      tertiary: "#eaf2ff",
       quaternary: "#182a47",
       banner: "rgba(0, 0, 0, 0.6)",
       projectBox: "rgba(0,0,0,0.7)"
@@ -19,16 +19,18 @@ const theme = {
     primary: "#DDDDDD",
     secondary: "#2b2b2b",
     tertiary: "#1c1c1c",
-    lightsourceBlue: '#090E1D',
-    lightsourceWhite: '#FEFCFE',
-    aalumPink: '#E13090',
-    aalumWhite: '#F9F9F9',
-    black: '#000000',
-    white: '#FFFFFF'
+    lightsourceBlue: "#090E1D",
+    lightsourceWhite: "#FEFCFE",
+    aalumPink: "#E13090",
+    aalumWhite: "#F9F9F9",
+    black: "#000000",
+    white: "#FFFFFF",
+    hummus: "#ffebd8",
+    hummusGray: "#262828"
   },
   fonts: {
     primary: "Raleway",
-    secondary: 'Damion',
+    secondary: "Damion",
     menu: "Damion",
     greeting: "Damion",
     name: "Raleway, serif",
